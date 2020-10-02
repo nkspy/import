d912a8c4425796a4f3e95992c50f3cea1aff53d7
nkspy <70144080+nkspy@users.noreply.github.com>
2020-10-03 00:40:03 +0530

    RPS trainer

---
+++ rps.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR = r"E:\ML workspace\RPS\rps_training"
+
+CATEGORIES = ["paper", "rock","scissors"]
+IMG_SIZE = 50
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR,category)  # create path to rps
+        print(path)
+        class_num = CATEGORIES.index(category)  # get the classification
+        print(class_num)
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_COLOR)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+
+create_training_data()
+
+# print(len(training_data))
+# print(np.shape(training_data))
+import random
+random.shuffle(training_data)
+
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+#print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+
+import pickle
+
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+
+# pickle_in = open("X.pickle","rb")
+# X = pickle.load(pickle_in)
+#
+# pickle_in = open("y.pickle","rb")
+# y = pickle.load(pickle_in)
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+import pickle
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+
+model.add(Flatten())
+
+model.add(Dense(64))
+
+model.add(Dense(3))
+model.add(Activation('softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy'
+                   '',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=16, epochs=10, validation_split=0.3)
+model.save("rps_sentdex.model")
